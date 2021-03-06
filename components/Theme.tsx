8b6a1f8abffc5d693a3bc953c7fb829d8db28b80
Antony Kwok <akwok@utilitywarehouse.co.uk>
2021-03-06 10:31:28 +0000

    Use injectFirst prop

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,9 +1,20 @@
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import React from 'react';
-import theme from '../config/theme';
+import {
+  createGenerateClassName,
+  createMuiTheme,
+  StylesProvider,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import React from "react";
+import theme from "../config/theme";
+
+const generateClassName = createGenerateClassName({ disableGlobal: true });
 
 const Theme: React.FC = (props) => (
-  <ThemeProvider theme={createMuiTheme(theme)}>{props.children}</ThemeProvider>
+  <StylesProvider generateClassName={generateClassName}>
+    <ThemeProvider theme={createMuiTheme(theme)}>
+      {props.children}
+    </ThemeProvider>
+  </StylesProvider>
 );
 
 export default Theme;
