d0240560fc16d8ddac0bcf539d7f4e122841fcca
Antony Kwok <akwok@utilitywarehouse.co.uk>
2021-03-16 10:31:45 +0000

    Redirect to error page if success=false on payment endpoint

--- components/TertiaryButton/index.tsx
+++ components/TertiaryButton/index.tsx
@@ -4,17 +4,19 @@ import styles from "./styles.module.css";
 
 interface TertiaryButtonProps extends HTMLAttributes<HTMLElement> {
   href?: string;
+  target?: string;
 }
 
 export const TertiaryButton: FunctionComponent<TertiaryButtonProps> = ({
   children,
   className,
   href,
+  target,
   ...rest
 }) => (
   <span className={classNames(styles.tertiaryButton, className)} {...rest}>
     {href ? (
-      <a href={href} className={styles.link}>
+      <a href={href} target={target} className={styles.link}>
         {children}
       </a>
     ) : (
