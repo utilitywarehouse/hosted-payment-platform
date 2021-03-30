62391a58a9550bed900c2d46b4a19cfb7efab820
Antony Kwok <akwok@utilitywarehouse.co.uk>
2021-03-30 10:48:52 +0100

    Generate custom build ID

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const execSync = require("child_process").execSync;
 const withImages = require('next-images')
+
+const lastCommitCommand = "git rev-parse HEAD";
 
 module.exports = withImages({
   publicRuntimeConfig: {
@@ -6,5 +9,8 @@ module.exports = withImages({
     GRAPHQL_URL: process.env.GRAPHQL_URL || 'https://finance-graphql.dev.aws.uw.systems/query',
     SPREEDLY_ENVIRONMENT_KEY: process.env.SPREEDLY_ENVIRONMENT_KEY || "LncsIjDAoJEsJdp51wFFONjRkzb",
     MIXPANEL_TOKEN: process.env.MIXPANEL_TOKEN || "c4f9c42eec07534c28a8903a7c471f82"
-  }
+  },
+  generateBuildId: async () => {
+    return execSync(lastCommitCommand).toString().trim();
+  },
 });
